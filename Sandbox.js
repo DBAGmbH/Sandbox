1dd07f9035a210102c16243fc00c2e633bb690be
rithbuster <denis.bauer1@gmx.net>
2019-02-07 16:26:22 +0100

    test

---
+++ Sandbox.js
@@ -0,0 +1,9 @@
+for (var Nerv = 0;Nerv < 5; Nerv++ ) {
+    alert("Nerv");
+     if (Nerv == 4) {
+         var weiternerven = confirm("Willste weiter genervt werden?");
+         if (weiternerven == false) {
+             break;
+         }
+     }
+}
